d2e4737021e62ec74b47e8fc9e3f841c7861f9d5
epikaigle444 <epikaigle444@gmail.com>
2026-07-21 18:00:32 +0200

    Update site URL and fix SearchModal import

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import pagefind from 'astro-pagefind';
 
 export default defineConfig({
-  site: 'https://lesfousdubus.sbs',
+  site: 'https://test-site-les-fous-du-bus.epikaigle444.workers.dev',
   output: 'static',
   integrations: [
     react(),
